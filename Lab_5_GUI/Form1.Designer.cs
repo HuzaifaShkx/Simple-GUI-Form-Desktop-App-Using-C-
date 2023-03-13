346d0b053396d2540ab6f964f364bd3b5396b440
HuzaifaShkx <80284490+HuzaifaShkx@users.noreply.github.com>
2023-03-13 22:22:32 +0500

    Add project files.

---
+++ Lab_5_GUI/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Lab_5_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmpno = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtBsal = new System.Windows.Forms.TextBox();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDeduction = new System.Windows.Forms.TextBox();
+            this.txtNSal = new System.Windows.Forms.TextBox();
+            this.txtAsal = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emp No";
+            // 
+            // txtEmpno
+            // 
+            this.txtEmpno.Location = new System.Drawing.Point(178, 24);
+            this.txtEmpno.Name = "txtEmpno";
+            this.txtEmpno.Size = new System.Drawing.Size(183, 20);
+            this.txtEmpno.TabIndex = 1;
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(178, 67);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(183, 20);
+            this.txtEmpName.TabIndex = 2;
+            // 
+            // txtBsal
+            // 
+            this.txtBsal.Location = new System.Drawing.Point(178, 112);
+            this.txtBsal.Name = "txtBsal";
+            this.txtBsal.Size = new System.Drawing.Size(183, 20);
+            this.txtBsal.TabIndex = 3;
+            // 
+            // txtTax
+            // 
+            this.txtTax.Location = new System.Drawing.Point(178, 194);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.Size = new System.Drawing.Size(183, 20);
+            this.txtTax.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Basic Salary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tax 6%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Deduction";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(74, 244);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Net Salary";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(74, 284);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Annual Salary";
+            // 
+            // txtDeduction
+            // 
+            this.txtDeduction.Location = new System.Drawing.Point(178, 154);
+            this.txtDeduction.Name = "txtDeduction";
+            this.txtDeduction.Size = new System.Drawing.Size(183, 20);
+            this.txtDeduction.TabIndex = 11;
+            // 
+            // txtNSal
+            // 
+            this.txtNSal.Location = new System.Drawing.Point(178, 237);
+            this.txtNSal.Name = "txtNSal";
+            this.txtNSal.Size = new System.Drawing.Size(183, 20);
+            this.txtNSal.TabIndex = 12;
+            // 
+            // txtAsal
+            // 
+            this.txtAsal.Location = new System.Drawing.Point(178, 277);
+            this.txtAsal.Name = "txtAsal";
+            this.txtAsal.Size = new System.Drawing.Size(183, 20);
+            this.txtAsal.TabIndex = 13;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCalculate.Location = new System.Drawing.Point(483, 31);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 23);
+            this.btnCalculate.TabIndex = 14;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnClear.Location = new System.Drawing.Point(483, 87);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnExit.Location = new System.Drawing.Point(483, 151);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(120, 23);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtAsal);
+            this.Controls.Add(this.txtNSal);
+            this.Controls.Add(this.txtDeduction);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.txtBsal);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtEmpno);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Employee Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmpno;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtBsal;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDeduction;
+        private System.Windows.Forms.TextBox txtNSal;
+        private System.Windows.Forms.TextBox txtAsal;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Lab_5_GUI/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_5_GUI
+{
+    public partial class Form1 : Form
+    {
+        float bsal, deduction, tax, nsal, asal;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (txtEmpno.Text == string.Empty)
+            {
+                MessageBox.Show("Employee no. is Missing");
+            }
+            else if (txtEmpName.Text == string.Empty)
+            {
+                MessageBox.Show("Employee name is Missing");
+            }
+            else if (txtBsal.Text == string.Empty)
+            {
+                MessageBox.Show("Employee Salary is Missing");
+            }
+            else if (txtDeduction.Text == string.Empty)
+            {
+                MessageBox.Show("Employee Deduction is Missing");
+            }
+            else
+            {
+                try { 
+                    bsal=float.Parse(txtBsal.Text);
+                    deduction=float.Parse(txtDeduction.Text);
+                    tax = float.Parse((bsal * 0.06).ToString());
+                    txtTax.Text = tax.ToString();
+                    nsal = bsal - tax - deduction;
+                    txtNSal.Text=nsal.ToString();
+                    txtAsal.Text = (nsal * 12).ToString();
+                }
+                catch (Exception ec) {
+                    MessageBox.Show(ec.ToString());
+                }
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNSal.Text = string.Empty;
+            txtEmpno.Text = string.Empty;
+            txtEmpName.Text = string.Empty;
+            txtDeduction.Text = string.Empty;
+            txtAsal.Text = string.Empty;
+            txtBsal.Text = string.Empty;
+            txtTax.Text = string.Empty;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
